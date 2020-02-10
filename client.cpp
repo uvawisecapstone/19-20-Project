2eb4473cab963fdd0c3ba6308cd0eb976dfd661a
uvawisecapstone <60196875+uvawisecapstone@users.noreply.github.com>
2020-02-10 16:52:47 -0500

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,50 @@
+#include <pthread.h>
+#include "client_wss.hpp"
+#include "server_wss.hpp"
+#include <future>
+
+using namespace std;
+
+using WssClient = SimpleWeb::SocketClient<SimpleWeb::WSS>;
+
+int main() {
+  WssClient client("localhost:8080/echo", false); // Second Client() parameter set to false: no certificate verification
+  client.on_message = [](shared_ptr<WssClient::Connection> connection, shared_ptr<WssClient::InMessage> in_message) {
+    cout << "Client: Message received: \"" << in_message->string() << "\"" << endl;
+    
+    string out_message;
+	cout << "Enter message ('Close' to close connection): ";
+	cin >> out_message;
+	
+	if(out_message != "Close"){
+	  cout << "Client: Sending message: \"" << out_message << "\"" << endl;
+	  connection->send(out_message);
+    }
+    else{
+	  cout << "Client: Sending close connection" << endl;
+	  connection->send_close(1000);
+	}
+  };
+
+  client.on_open = [](shared_ptr<WssClient::Connection> connection) {
+    cout << "Client: Opened connection" << endl;
+    
+    string out_message;
+	cout << "Enter message ('Close' to close connection): ";
+	cin >> out_message;
+	cout << "Client: Sending message: \"" << out_message << "\"" << endl;
+	connection->send(out_message);
+  };
+
+  client.on_close = [](shared_ptr<WssClient::Connection> connection, int status, const string & /*reason*/) {
+    cout << "Client: Closed connection with status code " << status << endl;
+  };
+
+  // See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
+  client.on_error = [](shared_ptr<WssClient::Connection> /*connection*/, const SimpleWeb::error_code &ec) {
+    cout << "Client: Error: " << ec << ", error message: " << ec.message() << endl;
+  };
+
+  client.start();
+  
+}
---
+++ server.cpp
@@ -0,0 +1,110 @@
+#include <pthread.h>
+#include "client_wss.hpp"
+#include "server_wss.hpp"
+#include <future>
+
+using namespace std;
+
+using WssServer = SimpleWeb::SocketServer<SimpleWeb::WSS>;
+
+int main() {
+  // WebSocket Secure (WSS)-server at port 8080 using 1 thread
+  WssServer server("server.crt", "server.key");
+  server.config.port = 8080;
+
+  auto &echo = server.endpoint["^/echo/?$"];
+
+  echo.on_message = [](shared_ptr<WssServer::Connection> connection, shared_ptr<WssServer::InMessage> in_message) {
+    auto out_message = in_message->string();
+
+    cout << "Server: Message received: \"" << out_message << "\" from " << connection.get() << endl;
+
+	if(out_message == "send_file"){
+		out_message = "Here is the file";
+	}
+	else{
+		out_message = "Invalid Request";
+	}
+	
+	cout << "Server: Sending message \"" << out_message << "\" to " << connection.get() << endl;
+
+	// connection->send is an asynchronous function
+	connection->send(out_message, [](const SimpleWeb::error_code &ec) {
+	if(ec) {
+		cout << "Server: Error sending message. " <<
+			// See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
+			"Error: " << ec << ", error message: " << ec.message() << endl;
+	}
+	});
+
+    // Alternatively use streams:
+    // auto out_message = make_shared<WssServer::OutMessage>();
+    // *out_message << in_message->string();
+    // connection->send(out_message);
+  };
+
+  echo.on_open = [](shared_ptr<WssServer::Connection> connection) {
+    cout << "Server: Opened connection " << connection.get() << endl;
+  };
+
+  // See RFC 6455 7.4.1. for status codes
+  echo.on_close = [](shared_ptr<WssServer::Connection> connection, int status, const string & /*reason*/) {
+    cout << "Server: Closed connection " << connection.get() << " with status code " << status << endl;
+  };
+
+  // Can modify handshake response header here if needed
+  echo.on_handshake = [](shared_ptr<WssServer::Connection> /*connection*/, SimpleWeb::CaseInsensitiveMultimap & /*response_header*/) {
+    return SimpleWeb::StatusCode::information_switching_protocols; // Upgrade to websocket
+  };
+
+  // See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
+  echo.on_error = [](shared_ptr<WssServer::Connection> connection, const SimpleWeb::error_code &ec) {
+    cout << "Server: Error in connection " << connection.get() << ". "
+         << "Error: " << ec << ", error message: " << ec.message() << endl;
+  };
+
+  // Example 2: Echo thrice
+  // Demonstrating queuing of messages by sending a received message three times back to the client.
+  // Concurrent send operations are automatically queued by the library.
+  // Test with the following JavaScript:
+  //   var wss=new WebSocket("wss://localhost:8080/echo_thrice");
+  //   wss.onmessage=function(evt){console.log(evt.data);};
+  //   wss.send("test");
+  auto &echo_thrice = server.endpoint["^/echo_thrice/?$"];
+  echo_thrice.on_message = [](shared_ptr<WssServer::Connection> connection, shared_ptr<WssServer::InMessage> in_message) {
+    auto out_message = make_shared<string>(in_message->string());
+
+    connection->send(*out_message, [connection, out_message](const SimpleWeb::error_code &ec) {
+      if(!ec)
+        connection->send(*out_message); // Sent after the first send operation is finished
+    });
+    connection->send(*out_message); // Most likely queued. Sent after the first send operation is finished.
+  };
+
+  // Example 3: Echo to all WebSocket Secure endpoints
+  // Sending received messages to all connected clients
+  // Test with the following JavaScript on more than one browser windows:
+  //   var wss=new WebSocket("wss://localhost:8080/echo_all");
+  //   wss.onmessage=function(evt){console.log(evt.data);};
+  //   wss.send("test");
+  auto &echo_all = server.endpoint["^/echo_all/?$"];
+  echo_all.on_message = [&server](shared_ptr<WssServer::Connection> /*connection*/, shared_ptr<WssServer::InMessage> in_message) {
+    auto out_message = in_message->string();
+
+    // echo_all.get_connections() can also be used to solely receive connections on this endpoint
+    for(auto &a_connection : server.get_connections())
+      a_connection->send(out_message);
+  };
+
+  // Start server and receive assigned port when server is listening for requests
+  promise<unsigned short> server_port;
+  thread server_thread([&server, &server_port]() {
+    // Start server
+    server.start([&server_port](unsigned short port) {
+      server_port.set_value(port);
+    });
+  });
+  cout << "Server listening on port " << server_port.get_future().get() << endl << endl;
+
+  server_thread.join();
+}
